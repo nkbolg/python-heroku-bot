c9d299666c55f4041ee3c64fcf87d84080c350aa
Nikolay Bolgurtcev <nikbol@transas.com>
2017-09-12 19:39:29 +0300

    basic heroku files

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import telegram
+import logging
 from telegram.ext import Updater, CommandHandler
 
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 def on_start(bot, update):
-    update.message.reply_text("Hello, ", update.message.from_user.name)
+    try:
+        mes = update.message
+        print(mes)
+        user = mes.from_user;
+        print(user)
+        mes.reply_text(f"Hello, {user.name}")
+    except Exception as ex:
+        print(ex)
 
 token = ''
-update = Updater(token)
+updater = Updater(token)
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('start', on_start))
+updater.start_polling()
+updater.idle()
